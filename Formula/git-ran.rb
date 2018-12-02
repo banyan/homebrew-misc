8ff9a0e7c2af2c400f727d33fbe524b6251d7d5a
Kohei Hasegawa <ameutau@gmail.com>
2018-12-02 17:30:48 +0900

    Fix url

--- Formula/git-ran.rb
+++ Formula/git-ran.rb
@@ -1,7 +1,7 @@
 class GitRan < Formula
   desc     "Commit with last command you ran"
   homepage "https://github.com/banyan/git-ran"
-  url      "https://github.com/banyan/git-ran/archive/1.0.0.zip"
+  url      "https://github.com/banyan/git-ran/archive/1.0.0.tar.gz"
   sha256   "dc84e7c3525b4dcaf699e18d7d04521e0f83b5359dfb1ff8e7e8fe947d6ee35e"
 
   def install
