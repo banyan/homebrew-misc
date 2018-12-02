10ff1fb38c50757d30929330307e568669ef314c
Kohei Hasegawa <ameutau@gmail.com>
2018-12-02 19:49:16 +0900

    Update formula

--- Formula/git-ran.rb
+++ Formula/git-ran.rb
@@ -1,8 +1,8 @@
 class GitRan < Formula
   desc     "Commit with last command you ran"
   homepage "https://github.com/banyan/git-ran"
-  url      "https://github.com/banyan/git-ran/archive/1.0.3.tar.gz"
-  sha256   "6aa84ee14bc3fdc368972920dae71b8802da4a62e6e4cfe1438b8666eac9aec0"
+  url      "https://github.com/banyan/git-ran/archive/1.0.4.tar.gz"
+  sha256   "62208c7a2156511ebf49b23e01460e8b23837a646d461dec8a18d5fde6e2a68c"
 
   def install
     bin.install "git-ran"
